b6eaa3e9fcf8bea33d75847dcff8547e755849e5
ashita phulwani <ashita.phulwani@simformsolutions.com>
2025-05-05 13:43:04 +0530

    feat(atoms): Add index file

---
+++ src/components/atoms/index.tsx
@@ -0,0 +1,17 @@
+import ButtonAtom from './Button/button';
+import ImageUploadAtom from './Image/Image Upload/imageUpload';
+import InputAtom from './Input/input';
+import SwitchAtom from './Switch/switch';
+import ImageAtom from './Image/Image Display/image';
+/**
+ * Exports atomic components for reuse across the application.
+ *
+ * @module Atoms
+ * This module provides reusable atomic components.
+ * ButtonAtom - A reusable button component.
+ * ImageUploadAtom - A reusable image upload component.
+ * InputAtom - A reusable input field component.
+ * SwitchAtom - A reusable toggle switch component.
+ * ImageAtom - A reusable image component.
+ */
+export { ButtonAtom, ImageUploadAtom, InputAtom, SwitchAtom, ImageAtom };
